0858844ddc78c166388b3a6a18cb02ba5575de5a
sav0l <sitnikovaiq@yandex.ru>
2022-12-09 20:10:54 +0300

    we are going to academy

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def main():
                                                                     "or csv:path to csv default=csv")
     parser.add_argument('-p', '--port', type=str, default="443-443", help="ports for scan default=443-443")
     parser.add_argument('-I', '--interval', type=int, default=24, help="scan interval in hours default=24")
+    parser.add_argument('-I', '--interval', type=int, default=24, help="scan interval in hours default=24")
     parser.add_argument('ScanDateTime', type=str, help='date and time of the first scan (DD.MM.YY_HH:MM)')
     parser.add_argument('-t', '--token', type=str, default="5894305427:AAE03pvAh-6u9p3nftQzYHlyx5E2Ra9GekM",
                         help="telegram bot token")
     my_namespace = parser.parse_args()
-
 
 
 if __name__ == '__main__':
